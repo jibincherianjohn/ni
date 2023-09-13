cac48a2418967e5cb5ef3a8cdbe9003c300dbea2
jibincherianjohn <jibinjohn518@gmail.com>
2023-09-13 15:57:31 +0530

    new

---
+++ cal.js
@@ -0,0 +1,33 @@
+function displayData(data){
+    // w.value+=data
+    // e.value+=data
+    // r.value+=data
+    // t.value+=data
+    // y.value+=data
+    // yu.value+=data
+    // nn.value+=data
+    // u.value+=data
+    // i.value+=data
+    // p.value+=data
+    // h.value+=data
+    // ll.value+=data
+    // o.value+=data
+    // a.value+=data
+    // g.value+=data
+    // j.value+=data
+    // s.value+=data
+    // yt.value+=data
+    // ww.value+=data
+    qq.value+=data
+    
+}
+function allClear(){
+    qq.value=""
+}
+function findOut(){
+    qq.value=eval(qq.value)
+}
+
+function backSpace(){
+    qq.value= qq.value.slice(0,-1)
+}
